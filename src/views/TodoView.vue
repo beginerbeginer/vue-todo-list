fc541084bdad917afbb9658af1571f17350735ee
michael <prowebengineer@gmail.com>
2024-03-12 14:38:38 +0900

    feat: 【Vue3入門】実践を通じて学ぶTODO List #6

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -6,20 +6,36 @@ const todos = ref([
     { id: 2, title: '筋トレ', done: false },
     { id: 3, title: '買い物', done: true },
 ]);
+
+const newTodo = ref('');
+
+const addTodo = () => {
+    console.log('addTodo', newTodo.value);
+    const todo = {
+        id: todos.value.length + 1,
+        title: newTodo.value,
+        done: false,
+    };
+    todos.value.push(todo);
+    newTodo.value = '';
+};
 </script>
 <template>
     <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16">
         <div class="px-4 py-2">
             <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
         </div>
-        <form class="w-full max-w-sm mx-auto px-4 py-2">
+        <form class="w-full max-w-sm mx-auto px-4 py-2"
+              @submit.prevent="addTodo"
+        >
             <div class="flex items-center border-b-2 border-teal-500 py-2">
                 <input
+                    v-model="newTodo"
                     class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                     type="text" placeholder="Add a task">
                 <button
                     class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                    type="button">
+                    type="submit">
                     Add
                 </button>
             </div>
